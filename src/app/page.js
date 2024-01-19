7bdf98c6d1ea24c80a81c47edd63ecec0f46722c
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2024-01-19 17:46:27 +0530

    [feat]: Add chip input component

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import userData from "@/data/userData.json";
+import React from "react";
+import Image from "next/image";
+
+const Home = () => {
+  const [users, setUsers] = React.useState(userData.usersData);
+  const [selected, setSelected] = React.useState([]);
+  const [val, setVal] = React.useState("");
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  // Function to handle selecting a user from the list
+  const handleSelectUser = React.useCallback(
+    (user) => {
+      setSelected((prev) => [...prev, user]); // Add user to selected list
+      const remainUsers = users.filter((item) => item.id !== user.id); // Remove user from available list
+      setUsers(remainUsers);
+      setVal("");
+    },
+    [setSelected, setUsers, users]
+  );
+
+  // Function to handle removing a user from the selected list
+  const handleRemoveUser = React.useCallback(
+    (user) => {
+      const remainUsers = selected.filter((item) => item.id !== user.id); // Remove user from selected list
+      setSelected(remainUsers);
+      setUsers((prev) => [...prev, user]); // Add user back to available list
+    },
+    [setSelected, setUsers, selected]
+  );
+
+  // Filtered users based on search input, using useMemo for memoization
+  const filteredUsers = React.useMemo(() => {
+    return users.filter((user) =>
+      user.name.toLowerCase().includes(val.toLowerCase())
+    );
+  }, [users, val]);
+
+  // handle closing the user list when clicking outside
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        !event.target.closest("#container") &&
+        !event.target.closest("#listContainer")
+      ) {
+        setIsVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isVisible]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="grid place-items-center my-10 w-full max-w-3xl mx-auto">
+      <section className="w-full">
+        <h1 className="text-center mb-10 text-blue-600">Pick Users</h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section>
+          <ul className="flex items-center flex-wrap gap-2 w-full border-b-2 border-b-blue-800">
+            {selected.map((user) => (
+              <li
+                key={user.id}
+                className="flex items-center gap-1 px-2 py-1 border bg-gray-100 rounded-full mb-1"
+              >
+                <Image
+                  src={user.src}
+                  alt={user.name}
+                  width={18}
+                  height={18}
+                  className="aspect-square rounded-full"
+                />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <span className="text-xs">{user.name}</span>
+                <button onClick={() => handleRemoveUser(user)} className="ml-1">
+                  &#x2715;
+                </button>
+              </li>
+            ))}
+            <li className="relative" id="container">
+              <input
+                type="text"
+                value={val}
+                onFocus={() => setIsVisible(true)}
+                onChange={(e) => setVal(e.target.value)}
+                placeholder="Type user name to add"
+                className="outline-none p-2"
+              />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              {isVisible && users.length > 0 && (
+                <div
+                  className="border py-4 w-full min-w-max absolute left-0 top-10 bg-white"
+                  id="listContainer"
+                >
+                  <ul className="max-h-60 overflow-y-scroll">
+                    {filteredUsers.map((user) => (
+                      <li
+                        key={user.id}
+                        onClick={() => handleSelectUser(user)}
+                        className="flex items-center gap-2 hover:bg-slate-100 py-3 px-6 cursor-pointer"
+                      >
+                        <Image
+                          src={user.src}
+                          alt={user.name}
+                          width={25}
+                          height={25}
+                          className="aspect-square rounded-full"
+                        />
+                        <h2>{user.name}</h2>
+                        <span className="ml-2 text-gray-500">{user.email}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </li>
+          </ul>
+        </section>
+      </section>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
